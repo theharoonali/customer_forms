0639758859972e799018151efb23de0412476882
Muhammad Haroon <muhammadharoon1592@gmail.com>
2023-07-06 01:47:42 +0500

    first

---
+++ script.js
@@ -0,0 +1,324 @@
+
+  function previewImage1(event) {
+    var previewContainer = document.getElementById('imagePreview1');
+    previewContainer.innerHTML = '';
+
+    var files = event.target.files;
+
+    for (var i = 0; i < files.length; i++) {
+      var file = files[i];
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+        var thumbnail = document.createElement('img');
+        thumbnail.className = 'thumbnail';
+        thumbnail.src = e.target.result;
+        previewContainer.appendChild(thumbnail);
+      }
+
+      reader.readAsDataURL(file);
+    }
+  }
+  
+  function previewImage2(event) {
+    var previewContainer = document.getElementById('imagePreview2');
+    previewContainer.innerHTML = '';
+
+    var files = event.target.files;
+
+    for (var i = 0; i < files.length; i++) {
+      var file = files[i];
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+        var thumbnail = document.createElement('img');
+        thumbnail.className = 'thumbnail';
+        thumbnail.src = e.target.result;
+        previewContainer.appendChild(thumbnail);
+      }
+
+      reader.readAsDataURL(file);
+    }
+  }
+  
+  function previewImage3(event) {
+    var previewContainer = document.getElementById('imagePreview3');
+    previewContainer.innerHTML = '';
+
+    var files = event.target.files;
+
+    for (var i = 0; i < files.length; i++) {
+      var file = files[i];
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+        var thumbnail = document.createElement('img');
+        thumbnail.className = 'thumbnail';
+        thumbnail.src = e.target.result;
+        previewContainer.appendChild(thumbnail);
+      }
+
+      reader.readAsDataURL(file);
+    }
+  }
+  
+  function previewImage4(event) {
+    var previewContainer = document.getElementById('imagePreview4');
+    previewContainer.innerHTML = '';
+
+    var files = event.target.files;
+
+    for (var i = 0; i < files.length; i++) {
+      var file = files[i];
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+        var thumbnail = document.createElement('img');
+        thumbnail.className = 'thumbnail';
+        thumbnail.src = e.target.result;
+        previewContainer.appendChild(thumbnail);
+      }
+
+      reader.readAsDataURL(file);
+    }
+  }
+  
+  function previewImage5(event) {
+    var previewContainer = document.getElementById('imagePreview5');
+    previewContainer.innerHTML = '';
+
+    var files = event.target.files;
+
+    for (var i = 0; i < files.length; i++) {
+      var file = files[i];
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+        var thumbnail = document.createElement('img');
+        thumbnail.className = 'thumbnail';
+        thumbnail.src = e.target.result;
+        previewContainer.appendChild(thumbnail);
+      }
+
+      reader.readAsDataURL(file);
+    }
+  }
+
+  
+  function previewImage6(event) {
+    var previewContainer = document.getElementById('imagePreview6');
+    previewContainer.innerHTML = '';
+
+    var files = event.target.files;
+
+    for (var i = 0; i < files.length; i++) {
+      var file = files[i];
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+        var thumbnail = document.createElement('img');
+        thumbnail.className = 'thumbnail';
+        thumbnail.src = e.target.result;
+        previewContainer.appendChild(thumbnail);
+      }
+
+      reader.readAsDataURL(file);
+    }
+  }
+
+  
+  function previewImage7(event) {
+    var previewContainer = document.getElementById('imagePreview7');
+    previewContainer.innerHTML = '';
+
+    var files = event.target.files;
+
+    for (var i = 0; i < files.length; i++) {
+      var file = files[i];
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+        var thumbnail = document.createElement('img');
+        thumbnail.className = 'thumbnail';
+        thumbnail.src = e.target.result;
+        previewContainer.appendChild(thumbnail);
+      }
+
+      reader.readAsDataURL(file);
+    }
+  }
+
+  
+  function previewImage8(event) {
+    var previewContainer = document.getElementById('imagePreview8');
+    previewContainer.innerHTML = '';
+
+    var files = event.target.files;
+
+    for (var i = 0; i < files.length; i++) {
+      var file = files[i];
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+        var thumbnail = document.createElement('img');
+        thumbnail.className = 'thumbnail';
+        thumbnail.src = e.target.result;
+        previewContainer.appendChild(thumbnail);
+      }
+
+      reader.readAsDataURL(file);
+    }
+  }
+
+  
+  function previewImage9(event) {
+    var previewContainer = document.getElementById('imagePreview9');
+    previewContainer.innerHTML = '';
+
+    var files = event.target.files;
+
+    for (var i = 0; i < files.length; i++) {
+      var file = files[i];
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+        var thumbnail = document.createElement('img');
+        thumbnail.className = 'thumbnail';
+        thumbnail.src = e.target.result;
+        previewContainer.appendChild(thumbnail);
+      }
+
+      reader.readAsDataURL(file);
+    }
+  }
+
+  
+  function previewImage10(event) {
+    var previewContainer = document.getElementById('imagePreview10');
+    previewContainer.innerHTML = '';
+
+    var files = event.target.files;
+
+    for (var i = 0; i < files.length; i++) {
+      var file = files[i];
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+        var thumbnail = document.createElement('img');
+        thumbnail.className = 'thumbnail';
+        thumbnail.src = e.target.result;
+        previewContainer.appendChild(thumbnail);
+      }
+
+      reader.readAsDataURL(file);
+    }
+  }
+
+  
+  function previewImage11(event) {
+    var previewContainer = document.getElementById('imagePreview11');
+    previewContainer.innerHTML = '';
+
+    var files = event.target.files;
+
+    for (var i = 0; i < files.length; i++) {
+      var file = files[i];
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+        var thumbnail = document.createElement('img');
+        thumbnail.className = 'thumbnail';
+        thumbnail.src = e.target.result;
+        previewContainer.appendChild(thumbnail);
+      }
+
+      reader.readAsDataURL(file);
+    }
+  }
+
+  
+  function previewImage12(event) {
+    var previewContainer = document.getElementById('imagePreview12');
+    previewContainer.innerHTML = '';
+
+    var files = event.target.files;
+
+    for (var i = 0; i < files.length; i++) {
+      var file = files[i];
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+        var thumbnail = document.createElement('img');
+        thumbnail.className = 'thumbnail';
+        thumbnail.src = e.target.result;
+        previewContainer.appendChild(thumbnail);
+      }
+
+      reader.readAsDataURL(file);
+    }
+  }
+
+  
+  function previewImage13(event) {
+    var previewContainer = document.getElementById('imagePreview13');
+    previewContainer.innerHTML = '';
+
+    var files = event.target.files;
+
+    for (var i = 0; i < files.length; i++) {
+      var file = files[i];
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+        var thumbnail = document.createElement('img');
+        thumbnail.className = 'thumbnail';
+        thumbnail.src = e.target.result;
+        previewContainer.appendChild(thumbnail);
+      }
+
+      reader.readAsDataURL(file);
+    }
+  }
+
+  
+  function previewImage14(event) {
+    var previewContainer = document.getElementById('imagePreview14');
+    previewContainer.innerHTML = '';
+
+    var files = event.target.files;
+
+    for (var i = 0; i < files.length; i++) {
+      var file = files[i];
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+        var thumbnail = document.createElement('img');
+        thumbnail.className = 'thumbnail';
+        thumbnail.src = e.target.result;
+        previewContainer.appendChild(thumbnail);
+      }
+
+      reader.readAsDataURL(file);
+    }
+  }
+
+  function previewImage15(event) {
+    var previewContainer = document.getElementById('imagePreview15');
+    previewContainer.innerHTML = '';
+
+    var files = event.target.files;
+
+    for (var i = 0; i < files.length; i++) {
+      var file = files[i];
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+        var thumbnail = document.createElement('img');
+        thumbnail.className = 'thumbnail';
+        thumbnail.src = e.target.result;
+        previewContainer.appendChild(thumbnail);
+      }
+
+      reader.readAsDataURL(file);
+    }
+  }
